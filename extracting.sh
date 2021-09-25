fa0a5792e262cce05c309bfccc3ce0a4e259f281
Eisukeniho <g1744906@cc.kyoto-su.ac.jp>
2021-09-25 21:03:29 +0900

    未完

--- extracting.sh
+++ extracting.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 SRC=$(basename $1)
-DIR=$(dirname $1)
+#ディレクトリ上の全ての'~.class'を'SRC'に代入
+#ex) BackSlashPrinter.class
+#なくてもコンパイルできる
 
-groovyc -d $DIR $1
-groovy &
+
+DIR=$(dirname $1)
+#各ディレクトリを'DIR'に代入
+#ex) .//課題1/0001_1
+
+
+#$1には、ファイルが代入されている
+#ex) BackSlashPrinter.class
+
+#echo $SRC
+
+
+pochi $DIR/extracting_birthmarks.groovy $SRC
