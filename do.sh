94cd0557758fc8682697befa0800d908c54e785f
Eisukeniho <g1744906@cc.kyoto-su.ac.jp>
2021-09-25 19:13:07 +0900

    説明文を追加

--- do.sh
+++ do.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 SRC=$(basename $1)
-DIR=$(dirname $1)
+#ディレクトリ上の全ての'~.java'を'SRC'に代入
+#ex) BackSlashPrinter.java
 
-echo $1 1
-echo $DIR DIR
+
+DIR=$(dirname $1)
+#各ディレクトリを'DIR'に代入
+#ex) .//課題1/0001_1
+
+
+#$1には、javaファイルが代入されている
+#ex) BackSlashPrinter.java
+
 
 javac -d $DIR $1
 
